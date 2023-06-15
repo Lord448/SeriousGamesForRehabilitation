2e15c2294b71a6784224554ef5744e9c4d0f11cf
Lord448 <pedroeroca@outlook.com>
2023-06-14 19:58:32 -0600

    Last ladder working BLE

--- NotasBLE/ESPMCU/Arduino/ladder/ladder.ino
+++ NotasBLE/ESPMCU/Arduino/ladder/ladder.ino
@@ -1,6 +1,6 @@
 /**
  * @file ladder.ino
- * @author Pedro Rojo (pedroeroca@outlook.com)
+ * @author Pedro Rojo (pedroeroca@outlook.com); Lord448 @ github.com
  * @brief 
  * @version 0.1
  * @date 2023-06-08
@@ -14,19 +14,40 @@
 #include <BLE2902.h>
 #include <string.h>
 
+#define SERVER_NAME "UART Service"
+#define LED_PIN 2
+#define DEFAULT_THRESHOLD 500
+
+//Comment this line to use ESP32 the WROOM 32 pin version
+#define PIN30 
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+#define SERVICE_UUID           "670b1f26-0a44-11ee-be56-0242ac120002" // UART service UUID
+#define CHARACTERISTIC_UUID_RX "006e861c-0a45-11ee-be56-0242ac120002"
+#define CHARACTERISTIC_UUID_TX "058804de-0a45-11ee-be56-0242ac120002"
+
 BLEServer *pServer = NULL;
 BLECharacteristic *pTxCharacteristic;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
-uint8_t txValue = 0;
-char Buffer[10];
+char Buffer[4];
+uint32_t threshold;
+uint32_t touchPins[10];
+uint32_t touchGPIO[10] = {
+                    4,   //Touch pin 0
+                    0,   //Touch Pin 1
+                    2,   //Touch pin 2
+                    15,  //Touch pin 3
+                    13,  //Touch pin 4
+                    12,  //Touch pin 5
+                    14,  //Touch pin 6
+                    27,  //Touch pin 7
+                    33,  //Touch pin 8
+                    32}; //Touch pin 9
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
 
-#define SERVICE_UUID           "670b1f26-0a44-11ee-be56-0242ac120002" // UART service UUID
-#define CHARACTERISTIC_UUID_RX "006e861c-0a45-11ee-be56-0242ac120002"
-#define CHARACTERISTIC_UUID_TX "058804de-0a45-11ee-be56-0242ac120002"
+void sendData(char *buffer, BLECharacteristic *pTXCharacteristic);
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
@@ -52,51 +73,70 @@ class MyCallbacks: public BLECharacteristicCallbacks {
 
 
 void setup() {
-  Serial.begin(115200);
+    Serial.begin(115200);
+    /*
+    for(uint32_t i = 0; i < sizeof(touchPins); i++)
+        touchPins[i] = i;
+    */
+    pinMode(LED_PIN, OUTPUT);
 
-  // Create the BLE Device
-  BLEDevice::init("UART Service");
+    // Create the BLE Device
+    BLEDevice::init(SERVER_NAME);
 
-  // Create the BLE Server
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
+    // Create the BLE Server
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
 
-  // Create the BLE Service
-  BLEService *pService = pServer->createService(SERVICE_UUID);
+    // Create the BLE Service
+    BLEService *pService = pServer->createService(SERVICE_UUID);
 
-  // Create a BLE Characteristic
-  pTxCharacteristic = pService->createCharacteristic(
-										CHARACTERISTIC_UUID_TX,
-										BLECharacteristic::PROPERTY_NOTIFY
-									);
-                      
-  pTxCharacteristic->addDescriptor(new BLE2902());
+    // Create a BLE Characteristic
+    pTxCharacteristic = pService->createCharacteristic(
+                                        CHARACTERISTIC_UUID_TX,
+                                        BLECharacteristic::PROPERTY_NOTIFY
+                                    );
+                        
+    pTxCharacteristic->addDescriptor(new BLE2902());
 
-  BLECharacteristic *pRxCharacteristic = pService->createCharacteristic(
-											 CHARACTERISTIC_UUID_RX,
-											BLECharacteristic::PROPERTY_WRITE
-										);
+    BLECharacteristic *pRxCharacteristic = pService->createCharacteristic(
+                                                CHARACTERISTIC_UUID_RX,
+                                            BLECharacteristic::PROPERTY_WRITE
+                                        );
 
-  pRxCharacteristic->setCallbacks(new MyCallbacks());
+    pRxCharacteristic->setCallbacks(new MyCallbacks());
 
-  // Start the service
-  pService->start();
+    // Start the service
+    pService->start();
 
-  // Start advertising
-  pServer->getAdvertising()->start();
-  Serial.println("Waiting a client connection to notify...");
+    // Start advertising
+    pServer->getAdvertising()->start();
+    Serial.println("Waiting a client connection to notify...");
 }
 
 void loop() {
-
     if (deviceConnected) {
-        sprintf(Buffer, "%d", txValue);
-        sendData(Buffer);
-        txValue++;
+        if(digitalRead(LED_PIN) == 0)
+            digitalWrite(LED_PIN, 1);
+        /*
+        for(uint32_t i = 0; i < sizeof(touchGPIO); i++) {
+            #ifdef PIN30
+            if(i == 1)
+                continue;
+            #endif // PIN30
+            if(touchRead(touchGPIO[i]) > threshold)
+            {
+                sprintf(Buffer, "T%d", touchPins[i]);
+                delay(1);
+                sendData(Buffer, pTxCharacteristic);
+            }
+        }
+        */
 	}
 
     // disconnecting
     if (!deviceConnected && oldDeviceConnected) {
+        if(digitalRead(LED_PIN) == 1)
+            digitalWrite(LED_PIN, 0);
         delay(500); // give the bluetooth stack the chance to get things ready
         pServer->startAdvertising(); // restart advertising
         Serial.println("start advertising");
@@ -109,18 +149,21 @@ void loop() {
     }
 }
 
-
-void sendData(char *buffer) {
+/**
+ * @brief Send data to the desired BLE characteristic in UTF-8
+ * @note  a string finisher is sent at the end of the transmit
+ * @param buffer String data to send 
+ * @param pTXCharacteristic Characteristic that will be modified
+ */
+void sendData(char *buffer, BLECharacteristic *pTXCharacteristic) {
     char charTX;
     for(uint32_t i = 0; i <= strlen(buffer); i++) {
         if(i != strlen(buffer))
             charTX = buffer[i];
         else
             charTX = '\n';
-        pTxCharacteristic -> setValue((uint8_t *)&charTX, sizeof(uint8_t));
-        pTxCharacteristic -> notify();
-        delay(15); // bluetooth stack will go into congestion, if too many packets are sent
+        pTXCharacteristic -> setValue((uint8_t *)&charTX, sizeof(uint8_t));
+        pTXCharacteristic -> notify();
+        delay(15); // bluetooth stack will go into congestion, if too many packets are sent, 10ms min
     }
-
-    //Serial.println("Data has been sent");
 }
