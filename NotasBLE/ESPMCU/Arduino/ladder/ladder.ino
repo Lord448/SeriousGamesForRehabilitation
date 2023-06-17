9b498b16b69cae46c71840d56f98926c83bcf982
Lord448 <pedroeroca@outlook.com>
2023-06-16 19:22:04 -0600

    basic before visit ladder 0.1 ready

--- NotasBLE/ESPMCU/Arduino/ladder/ladder.ino
+++ NotasBLE/ESPMCU/Arduino/ladder/ladder.ino
@@ -4,25 +4,34 @@
  * @brief 
  * @version 0.1
  * @date 2023-06-08
- * 
+ * @todo Process the RX messsage to automate calibration of the sensors and other configs
  * @copyright Copyright (c) 2023
  */
-
+#include <stdio.h>
+#include <string.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
-#include <string.h>
 
 #define SERVER_NAME "UART Service"
 #define LED_PIN 2
-#define DEFAULT_THRESHOLD 500
+#define DEFAULT_THRESHOLD 60
 
-//Comment this line to use ESP32 the WROOM 32 pin version
+//This symbol disables the touch sensor checkout and prepare the raw data to calibrate the threshold via serial monitor
+//#define CALIBRATION_MODE
+
+#ifdef CALIBRATION_MODE
+//This symbol enable the data send to the bluetooth
+#define CALIBRATION_MODE_BT
+#endif
+
+//Comment this symbol to use ESP32 the WROOM 32 pin version
 #define PIN30 
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
+
 #define SERVICE_UUID           "670b1f26-0a44-11ee-be56-0242ac120002" // UART service UUID
 #define CHARACTERISTIC_UUID_RX "006e861c-0a45-11ee-be56-0242ac120002"
 #define CHARACTERISTIC_UUID_TX "058804de-0a45-11ee-be56-0242ac120002"
@@ -32,8 +41,9 @@ BLECharacteristic *pTxCharacteristic;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
 char Buffer[4];
-uint32_t threshold;
+uint32_t threshold = DEFAULT_THRESHOLD;
 uint32_t touchPins[10];
+bool wasTouched[10];
 uint32_t touchGPIO[10] = {
                     4,   //Touch pin 0
                     0,   //Touch Pin 1
@@ -45,7 +55,6 @@ uint32_t touchGPIO[10] = {
                     27,  //Touch pin 7
                     33,  //Touch pin 8
                     32}; //Touch pin 9
-
 
 void sendData(char *buffer, BLECharacteristic *pTXCharacteristic);
 
@@ -61,23 +70,26 @@ class MyServerCallbacks: public BLEServerCallbacks {
 
 class MyCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
-      std::string rxValue = pCharacteristic->getValue();
+        std::string rxValue = pCharacteristic->getValue();
 
-      if (rxValue.length() > 0) {
-        Serial.print("Received Value: ");
-        for (int i = 0; i < rxValue.length(); i++)
-          Serial.print(rxValue[i]);
-      }
+        if (rxValue.length() > 0) {
+            Serial.print("Received Value: ");
+            for (int i = 0; i < rxValue.length(); i++)
+            Serial.print(rxValue[i]);
+            //@todo handle message
+        }
     }
 };
 
 
 void setup() {
     Serial.begin(115200);
-    /*
-    for(uint32_t i = 0; i < sizeof(touchPins); i++)
+
+    for(uint32_t i = 0; i < 10; i++) {
         touchPins[i] = i;
-    */
+        wasTouched[i] = false;
+    }
+
     pinMode(LED_PIN, OUTPUT);
 
     // Create the BLE Device
@@ -117,20 +129,35 @@ void loop() {
     if (deviceConnected) {
         if(digitalRead(LED_PIN) == 0)
             digitalWrite(LED_PIN, 1);
-        /*
-        for(uint32_t i = 0; i < sizeof(touchGPIO); i++) {
-            #ifdef PIN30
-            if(i == 1)
-                continue;
-            #endif // PIN30
-            if(touchRead(touchGPIO[i]) > threshold)
-            {
-                sprintf(Buffer, "T%d", touchPins[i]);
-                delay(1);
-                sendData(Buffer, pTxCharacteristic);
-            }
+
+        
+        for(uint32_t i = 0; i < 10; i++) {
+            #ifndef CALIBRATION_MODE
+
+                #ifdef PIN30
+                if(i == 1)
+                    continue;
+                #endif // PIN30
+
+                if(touchRead(touchGPIO[i]) < threshold) { //User takes pin
+                    if(!wasTouched[i]) {
+                        sprintf(Buffer, "T%d", touchPins[i]);
+                        sendData(Buffer, pTxCharacteristic);
+                        wasTouched[i] = true;
+                    }
+                }
+                else if(wasTouched[i]) { //User drops pin
+                    wasTouched[i] = false;
+                }
+            #else
+                Serial.printf("Pin: %d, Value: %d \n", touchPins[i], touchRead(touchGPIO[i]));
+                    #ifdef CALIBRATION_MODE_BT
+                        static char pinval[25];
+                        sprintf(pinval, "Pin: %d, Value: %d \n", touchPins[i], touchRead(touchGPIO[i]));
+                        sendData(pinval, pTxCharacteristic);
+                    #endif // SEND_TO_BT
+            #endif // ENABLE_CALIBRATION
         }
-        */
 	}
 
     // disconnecting
