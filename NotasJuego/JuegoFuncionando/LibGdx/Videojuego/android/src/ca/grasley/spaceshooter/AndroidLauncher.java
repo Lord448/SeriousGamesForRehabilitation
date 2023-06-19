c72d5376f5cd202ca6e5e127d1e44b8a7798daeb
Lord448 <pedroeroca@outlook.com>
2023-06-19 15:57:08 -0600

    Androidx gradle modification AppCompat

--- NotasJuego/JuegoFuncionando/LibGdx/Videojuego/android/src/ca/grasley/spaceshooter/AndroidLauncher.java
+++ NotasJuego/JuegoFuncionando/LibGdx/Videojuego/android/src/ca/grasley/spaceshooter/AndroidLauncher.java
@@ -4,9 +4,17 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+import java.util.UUID;
+
 import ca.grasley.spaceshooter.JuegoCRIT_Game;
 
 public class AndroidLauncher extends AndroidApplication {
+
+	public static final UUID txChUUID = UUID.fromString("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
+	public static final UUID rxChUUID = UUID.fromString("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
+
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
