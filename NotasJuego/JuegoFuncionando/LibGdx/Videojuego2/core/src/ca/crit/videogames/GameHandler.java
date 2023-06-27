865f88620cead336f956523e4a6a6130b32f951a
Coffee448 <andy_may_hazi_mejoresamigas@hotmail.com>
2023-06-26 21:23:39 -0600

    variable iterator added at gamehandler class

--- NotasJuego/JuegoFuncionando/LibGdx/Videojuego2/core/src/ca/crit/videogames/GameHandler.java
+++ NotasJuego/JuegoFuncionando/LibGdx/Videojuego2/core/src/ca/crit/videogames/GameHandler.java
@@ -4,4 +4,5 @@ public class GameHandler {
     public static boolean filled = false;
     public static boolean stageReached = false;
     public static boolean btFilled = false;
+    public int iterator;
 }
