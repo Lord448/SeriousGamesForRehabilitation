292af719c173909cd8b7c8ddab56e3e3a0033b75
Coffee448 <andy_may_hazi_mejoresamigas@hotmail.com>
2023-07-08 18:26:05 -0600

    Food class added

---
+++ NotasJuego/JuegoFuncionando/LibGdx/Videojuego/core/src/ca/grasley/spaceshooter/Food.java
@@ -0,0 +1,34 @@
+package ca.grasley.spaceshooter;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Food {
+    private int width, height;
+    private int x, y;
+
+    private TextureRegion[] food;
+    private TextureRegion currentFood;
+    private Texture image;
+
+    public Food(int x, int y, int width, int height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        image = new Texture(Gdx.files.internal("Food/food.png"));
+        TextureRegion [][] tmp = TextureRegion.split(image, image.getWidth()/4, image.getHeight()/2);
+        food = new TextureRegion[8];
+        int j = 0, i = 0;
+        for(int w = 0; w< food.length; w++){
+            food[w] = tmp [j][i];
+            if(i >= 3){
+                i = 0;
+                j++;
+            }
+            i++;
+        }
+    }
+}
