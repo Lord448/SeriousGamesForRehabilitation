1cfeda24514ca4fd6bee5934f08ea8e81057b117
Coffee448 <andy_may_hazi_mejoresamigas@hotmail.com>
2023-07-18 16:43:59 -0600

    Animal doesn't skip steps in progress

--- NotasJuego/JuegoFuncionando/LibGdx/Videojuego/core/src/ca/grasley/spaceshooter/Animal.java
+++ NotasJuego/JuegoFuncionando/LibGdx/Videojuego/core/src/ca/grasley/spaceshooter/Animal.java
@@ -11,6 +11,9 @@ public class Animal {
     private float[] positions = new float[GameHandler.numTouchPins];
     private Texture animal_texture;
     private float currentPos;
+
+    int past = 0, current = 0;
+    boolean sequential = false;
     public Animal (int x, int y, int width, int height, int max_lim, int min_lim, float speed) {
         float positionSet = 0;
         this.x = x;
@@ -30,8 +33,14 @@ public class Animal {
     }
     public void render(final SpriteBatch batch){
         batch.draw(animal_texture, x, y, width, height);
-        currentPos = y;
+        touchPins();
+        isSequential();
+        if(sequential){
+            climb();
+        }
+    }
 
+    private void touchPins(){
         for(int i = 0; i < GameHandler.numTouchPins; i++) {
             if(Gdx.input.isKeyPressed(GameHandler.key[i])) {
                 GameHandler.touchPins[i] = true;
@@ -41,7 +50,10 @@ public class Animal {
                 }
             }
         }
+    }
 
+    private void climb(){
+        currentPos = y;
         for(int i = 0; i < GameHandler.numTouchPins; i++) {
             if(GameHandler.touchPins[i]) {
                 if(currentPos > positions[i]+GameHandler.animHysteresis)
@@ -61,5 +73,21 @@ public class Animal {
                 }
             }
         }
+    }
+
+    private void isSequential(){
+        for(int i = 0; i<GameHandler.numTouchPins; i++){
+            if(Gdx.input.isKeyPressed(GameHandler.key[i])) {
+                current = (int) positions[i];
+                if(current > past){
+                    sequential = true;
+                    System.out.println("mayor");
+                } else if (current < past) {
+                    sequential = false;
+                    System.out.println("menor");
+                }
+            }
+        }
+        past = current;
     }
 }
